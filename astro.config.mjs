5e86fa19aa3d5aa2ec643b7aa2e3f62e6ba6262f
nehalDIU <113442689+sheikhshariarnehal@users.noreply.github.com>
2026-06-14 00:44:54 +0600

    added new chanel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 export default defineConfig({
-  // Astro configuration
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  })
 });
